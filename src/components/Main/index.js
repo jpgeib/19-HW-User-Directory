0fcc63ea08b3de3c5ca52594b098cd345aed7ae5
James Geib <jpgeib2@gmail.com>
2020-05-22 12:35:26 -0700

    built and styled Nav and SearchBox components

---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataArea from "./DataArea";
+import "./style.css";
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div>
+                <DataArea />
+            </div>
+        );
+    }
+};
---
+++ src/components/SearchBox/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+SearchBox = ({ handleSearchChange }) => {
+    return (
+        <div className="searchbox">
+            <form className ="form-inline">
+                <input 
+                    className="form-control"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    onChange={e => handleSearchChange(e)}
+                />
+            </form>
+        </div>
+    );
+};
+
+export default SearchBox;
